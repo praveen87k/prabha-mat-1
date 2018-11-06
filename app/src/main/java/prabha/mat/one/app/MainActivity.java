f93228f9f1292c5b48f2bb39803594e32d637632
pravepra <praveen.k.prakash@nokia.com>
2018-11-06 15:54:30 +0530

    test from prabha

--- app/src/main/java/prabha/mat/one/app/MainActivity.java
+++ app/src/main/java/prabha/mat/one/app/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test 3
+        // test 3 from prabha
     }
 }
