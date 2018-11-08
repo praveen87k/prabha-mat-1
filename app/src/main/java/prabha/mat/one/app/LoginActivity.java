31c038a3aaf8da3ceca41a9121209cc92c17c4f3
pravepra <praveen.k.prakash@nokia.com>
2018-11-08 20:33:00 +0530

    creating basic account using firebase

---
+++ app/src/main/java/prabha/mat/one/app/LoginActivity.java
@@ -0,0 +1,39 @@
+package prabha.mat.one.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Password;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Email = (EditText)findViewById(R.id.etEmail);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button)findViewById(R.id.btnLoginMenu);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Email.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String emailID, String password){
+        if(emailID.equals("aaa") && password.equals("111")){
+            Intent intent = new Intent(LoginActivity.this, MatchesActivity.class);
+            startActivity(intent);
+        }
+    }
+}
--- app/src/main/java/prabha/mat/one/app/WelcomeScreenActivity.java
+++ app/src/main/java/prabha/mat/one/app/WelcomeScreenActivity.java
@@ -1,11 +1,17 @@
 package prabha.mat.one.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
+
+    private Button LoginMenu;
+    private Button CreateAccountMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,24 @@ public class WelcomeScreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_welcome_screen);
         getSupportActionBar().hide();
+
+        LoginMenu = (Button)findViewById(R.id.btnLoginMenu);
+        CreateAccountMenu = (Button)findViewById(R.id.btnCreateAccountMenu);
+
+        LoginMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        CreateAccountMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeScreenActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
