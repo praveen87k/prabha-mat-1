9ffa270310367fb4cecffaa41f3209f1167c1674
pravepra <praveen.k.prakash@nokia.com>
2018-11-10 08:19:53 +0530

    adding home page

---
+++ app/src/main/java/prabha/mat/one/app/Tab3Contacts.java
@@ -0,0 +1,16 @@
+package prabha.mat.one.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Contacts extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3_contacts, container, false);
+        return rootView;
+    }
+}
