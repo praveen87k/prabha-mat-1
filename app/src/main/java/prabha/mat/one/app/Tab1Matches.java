4dac7c9475b6a751fc1417ddecfbfb4386440ffa
pravepra <praveen.k.prakash@nokia.com>
2018-12-02 14:06:13 +0530

    displaying list of users in matches screen

--- app/src/main/java/prabha/mat/one/app/Tab1Matches.java
+++ app/src/main/java/prabha/mat/one/app/Tab1Matches.java
@@ -7,50 +7,85 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class Tab1Matches extends Fragment{
 
-    private TextView age, name;
-    private FirebaseAuth firebaseAuth;
     private FirebaseDatabase firebaseDatabase;
     private ProgressDialog progressDialog;
+    private ArrayList<String> userNames = new ArrayList<String>();
+    private ArrayList<String> userAges = new ArrayList<String>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab1_matches, container, false);
-
-        age = rootView.findViewById(R.id.tvProfileAge);
-        name = rootView.findViewById(R.id.tvProfileName);
-        firebaseAuth = FirebaseAuth.getInstance();
+        final View rootView = inflater.inflate(R.layout.tab1_matches, container, false);
         firebaseDatabase = FirebaseDatabase.getInstance();
         progressDialog = new ProgressDialog(getActivity());
         progressDialog.setMessage("Loading");
         progressDialog.show();
 
-        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("users");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
-                age.setText(userProfile.getUserAge());
-                name.setText(userProfile.getUserName());
+                userNames.clear();
+                userAges.clear();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String userName = ds.child("userName").getValue(String.class);
+                    userNames.add(userName);
+                    String userAge = ds.child("userAge").getValue(String.class);
+                    userAges.add(userAge);
+                }
+                ListView listView = (ListView)rootView.findViewById(R.id.lvUsers);
+                CustomAdapter customAdapter = new CustomAdapter();
+                listView.setAdapter(customAdapter);
                 progressDialog.dismiss();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 progressDialog.dismiss();
-                Toast.makeText(getActivity(), databaseError.getCode(), Toast.LENGTH_SHORT).show();
+//                commenting as an error is thrown during logout
+//                Toast.makeText(getActivity(), databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+
+            class CustomAdapter extends BaseAdapter{
+
+                @Override
+                public int getCount() {
+                    return userNames.size();
+                }
+
+                @Override
+                public Object getItem(int position) {
+                    return null;
+                }
+
+                @Override
+                public long getItemId(int position) {
+                    return 0;
+                }
+
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    convertView = getLayoutInflater().inflate(R.layout.matching_user, null);
+                    TextView userName = (TextView)convertView.findViewById(R.id.tvUserName);
+                    TextView userAge = (TextView)convertView.findViewById(R.id.tvUserAge);
+                    userName.setText(userNames.get(position));
+                    userAge.setText(userAges.get(position));
+                    return convertView;
+                }
             }
         });
 
